ac9d7c780e0e0befe82c6c70d929c3163abad28b
Felipe Muller <felipeavmuller@gmail.com>
2021-02-26 21:00:17 -0300

    resolvendo exercicios de 1 a 3

---
+++ exercises/bloco_4/dia_3/script.js
@@ -0,0 +1,54 @@
+// exercicio 1
+
+// let n = 7;
+// let row = [];
+// row.length = n;
+// let square = '';
+
+// for (let index = 0; index < n; index += 1) {
+//   row[index] = '*';
+//   square += row[index];
+// }
+
+// for (let index = 0; index < n; index += 1) {
+//   console.log(square);
+// }
+
+// exercicio 2
+
+// let n = 7;
+// let row = [];
+// row.length = n;
+// let square = '';
+
+// for (let index = 0; index < n; index += 1) {
+//   row[index] = '*';
+//   square += row[index];
+//   console.log(square);
+// }
+
+// exercicio 3
+
+// let n = 5;
+// let row = [];
+// row.length = n;
+// let square = '';
+// let n2 = n;
+
+// for (let jndex = 0; jndex < n; jndex += 1)
+// {
+//   for (let index = 0; index < n; index += 1) {
+//     if (index < n2 - 1 ) {
+//       row[index] = " ";
+//       square += row[index];
+//     } else {
+//       row[index] = '*';
+//       square += row[index];
+//     }
+//   }
+//   console.log(square);
+//   n2 -= 1
+//   square = '';
+// }
+
+// exercicio 4
